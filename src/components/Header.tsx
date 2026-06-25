275c7ddcea9919d5c62765951eaa89c92578162a
Roee Finkelstein <roeefinkelstein@Roees-MacBook-Pro.local>
2026-06-25 09:06:03 +0300

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function Header() {
+    return (
+        <div className='w-full h-32'>
+            <p>Products</p>
+            <p>About</p>
+        </div>
+    )
+}
